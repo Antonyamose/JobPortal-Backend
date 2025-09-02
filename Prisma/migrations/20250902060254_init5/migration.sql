fec14d2eb182b03794c062aeb0165ea3dcb100f3
Antonyamose <antonyamose3@gmail.com>
2025-09-02 11:34:44 +0530

    2u

---
+++ Prisma/migrations/20250902060254_init5/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Job` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Joblist` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Token` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "public"."Job";
+
+-- DropTable
+DROP TABLE "public"."Joblist";
+
+-- DropTable
+DROP TABLE "public"."Token";
